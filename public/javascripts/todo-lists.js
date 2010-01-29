16f098b9de8c1ac7d6d65559fc3516834ba5bae8
Ivana Vasilj <ivasilj@gmail.com>
2010-01-29 16:55:22 +0100

    ajax submit for new list creation

--- public/javascripts/todo-lists.js
+++ public/javascripts/todo-lists.js
@@ -1,13 +1,30 @@
 $(function(){
 
-  $('h1 + a').click(function(){  // new list form
-    $(this).hide();
-    $('form#new_todo_list').show();
+  $('h1 + a').click(function(){  // show new list form
+    toggleCreateNewListElements();
   });
 
   $('form#new_todo_list input[type="reset"]').click(function(){ // cancel new list creation
-    $('form#new_todo_list').hide();
-    $('h1 + a').show();
+    toggleCreateNewListElements();
   });
+
+  $('form#new_todo_list input[type="submit"]').click(function(){ // save new list
+    $.post(
+      $('form#new_todo_list').attr('action'),
+      $('form#new_todo_list').serialize(),
+      function(data){
+        $('body > ol').prepend('<li><h2>' + data.todo_list.name + '</h2></li>');
+      }
+    );
+
+    toggleCreateNewListElements();
+    return false;
+  });
+
+  var toggleCreateNewListElements = function(){ // toggle form and button - show one, hide another
+    $('input#todo_list_name').val('');
+    $('form#new_todo_list').toggle(); // there could be some troubles with jQuery toggle in IE
+    $('h1 + a').toggle();
+  };
 
 });
