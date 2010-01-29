0081a35c844197e91140c4987f3291c5c5adadbe
Ivana Vasilj <ivasilj@gmail.com>
2010-01-29 12:54:17 +0100

    form#new_todo_list cancel button behaviour

--- public/javascripts/todo-lists.js
+++ public/javascripts/todo-lists.js
@@ -1,8 +1,13 @@
 $(function(){
 
-  $('h1 + a').click(function(eventObject){  // new list form
-    $(this).remove();
-    $('#new_todo_list').show();
+  $('h1 + a').click(function(){  // new list form
+    $(this).hide();
+    $('form#new_todo_list').show();
+  });
+
+  $('form#new_todo_list input[type="reset"]').click(function(){ // cancel new list creation
+    $('form#new_todo_list').hide();
+    $('h1 + a').show();
   });
 
 });
