d4a43d853f860f5ea41c7520415569081cb9e7a7
Ivana Vasilj <ivasilj@gmail.com>
2010-01-30 19:51:16 +0100

    edit & delete action buttons - markup & style

--- public/javascripts/visual.js
+++ public/javascripts/visual.js
@@ -1,9 +1,19 @@
 $(function(){
 
+  $('input[type="submit"], input[type="reset"]').button();
+
   $('h1 + a').button({  // create a new list
     icons: { primary: 'ui-icon-circle-plus' }
   });
 
-  $('input[type="submit"], input[type="reset"]').button();
+  $('a.edit').button({
+    icons: { primary: 'ui-icon-pencil'},
+    text: false
+  });
+
+  $('a.del').button({
+    icons: { primary: 'ui-icon-trash'},
+    text: false
+  });
 
 });
