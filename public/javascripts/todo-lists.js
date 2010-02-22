423e969960440bc978d29a5593b968374af32e6d
Ivana Vasilj <ivasilj@gmail.com>
2010-02-22 20:22:20 -0300

    commenting out edit name link

--- public/javascripts/todo-lists.js
+++ public/javascripts/todo-lists.js
@@ -59,12 +59,14 @@ $(function(){
   }); // clear done items
 
 
+/*
   $('li[id^="todo_list"] > a').live('mouseenter', function(){
     $(this).children('span').css('visibility', 'visible');
   });
   $('li[id^="todo_list"] > a').live('mouseleave', function(){
     $(this).children('span').css('visibility', 'hidden');
   }); // list title hover
+*/
 
 
 /*
