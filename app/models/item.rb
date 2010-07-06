f5ee3b77f7106d49394312dfc90dd13cda5a0060
Mislav <mislav.marohnic@gmail.com>
2010-07-06 21:48:57 +0200

    change deprecated before_validation to new syntax

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :todo_list
 
-  before_validation_on_create :set_default_priority
+  before_validation :set_default_priority, :on => :create
   validates_presence_of :description, :priority, :todo_list_id
   validates_inclusion_of :priority, :in => %w(low normal high)
 
