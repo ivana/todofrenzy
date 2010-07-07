e00ed6b43b53771563d39e301e2390a0b78b9f07
Mislav <mislav.marohnic@gmail.com>
2010-07-07 17:43:29 +0200

    items refactor

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,13 @@
 class ItemsController < ApplicationController
 
   def create
-    @item = Item.create(params[:item])
-    render :partial => 'item', :locals => { :item => @item }
+    @item = Item.create params[:item]
+    render @item
   end
 
   def update
-    item = Item.find(params[:id])
-    item.done = !!params[:done]
-    item.save
+    item = Item.find params[:id]
+    item.update_attributes params[:item]
     render :json => item
   end
 
