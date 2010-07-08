47085f536fa411389f725373875148ea5adc09d4
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-07-09 00:32:43 +0200

    exclude Analytics code in development

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,5 +3,18 @@ module ApplicationHelper
   def mac?
     request.user_agent =~ /\bMac OS\b/
   end
+  
+  def analytics_code
+    <<-CODE
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+    try {
+    var pageTracker = _gat._getTracker("UA-5355510-2");
+    pageTracker._trackPageview();
+    } catch(err) {}
+    CODE
+  end
 
 end
