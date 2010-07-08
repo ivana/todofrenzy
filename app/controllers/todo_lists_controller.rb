ac15843ceada9fd1229b80c2af103beefb08d4ef
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-07-08 20:56:29 +0200

    `destroy` bug in Rails now fixed

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -17,18 +17,15 @@ class TodoListsController < ApplicationController
   end
 
   def destroy
-    todo_list = current_user.todo_lists.find params[:id]
-    todo_list.destroy
+    todo_list = current_user.todo_lists.destroy params[:id]
+    
     render :json => todo_list
   end
 
   # clear done items
   def clear
     todo_list = TodoList.find params[:id]
-    # FIXME: replace this with `destroy_all` once it's fixed in Rails
-    done_items = todo_list.items.done
-    items = done_items.all(:select => :id)
-    done_items.delete_all
+    done_items = todo_list.items.done.destroy_all
     
     render :json => items
   end
