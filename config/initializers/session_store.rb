9817ca919de90763a956fd8373644a19f4474eb6
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-12-19 18:04:44 +0100

    set session cookie to last for 14 days

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-Todaslistas::Application.config.session_store :cookie_store, :key => '_todofrenzy_session'
+Todaslistas::Application.config.session_store :cookie_store,
+  :key => '_todofrenzy_session', :expire_after => 14.days
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
